661b545fd3e4e717cd70f2668f0a05e890903525
Yash708262 <yash708262@gmail.com>
2025-09-17 17:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+const url = document.getElementById('url');
+const btn = document.getElementById('button')
+btn.addEventListener('click', (e) => {
+     e.preventDefault();
+    url.textContent='https://d3dyfaf3iutrxo.cloudfront.net/thumbnail/assignment/question/8bb3b47db47841178b670717ea3c74f9.png'
+})
